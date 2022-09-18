e42f287bf42c190cab1a783cd1af37bcf943db4e
Jannatul Ferdous <jannatul.ferdous@stonybrook.edu>
2022-09-18 19:26:25 -0400

    New changes

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template
+ 
+app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello world, WWW!'
+
+@app.route('/')
+def homepage():
+    return render_template('p1_homepage_withCSS.html')
+
+@app.route('/exercise')
+def homepage_exercise():
+    return render_template('p2_homepage_withCSS.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=80)
+
+
 
